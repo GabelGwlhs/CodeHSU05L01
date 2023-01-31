6b9a72c662e66bf6ee24192d23a8b7dfd40d8842
Gracie Gabel <GabelG@WLHS.wlwv.K12.or.us>
2023-01-31 09:22:31 -0800

    hu

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -2,14 +2,27 @@ import java.text.DecimalFormat;
 public class Rectangle { //make public for CS A
 
     //instance variables
-
+    private double width;
+    private double height;
+    private double area;
+    private double permiter;
     //default constructor (make public for CS A)
 
+    public Rectangle() {
+        width = 1.0;
+        height = 1.0;
+    }
 
     //overloaded constructor (with parameters)
+    public Rectangle(double width, double height) {
 
 
-
+    width =w;
+    height=h;
+}
+    public Rectangle(double width){
+        this.width = width;//allows for this instance of the object to change
+    }
     //accessors
 
 
