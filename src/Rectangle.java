b2eff6626979b8fe7838086748a9acfbc904ba16
Gracie Gabel <GabelG@WLHS.wlwv.K12.or.us>
2023-02-06 09:19:07 -0800

    hu

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -14,25 +14,53 @@ public class Rectangle { //make public for CS A
     }
 
     //overloaded constructor (with parameters)
-    public Rectangle(double width, double height) {
+    public Rectangle(double w, double h) {
 
 
-    width =w;
-    height=h;
+    width =1.0;
+    height=1.0;
+    if(w>0){
+        width=
+    }
 }
     public Rectangle(double width){
         this.width = width;//allows for this instance of the object to change
     }
     //accessors
-
+    public double getPermiter(){
+        return permiter;
+    }
+    /**
+     * @de
+     * @param nw
+     */
 
 
     //mutators
+    public void setWidth(double nw){
+        width = nw;
+    }
 
+    /**
+     * modifies the height of the rectangle
+     * @param nh
+     */
+    public void setHeight(double nh){
+        height = nh;
+    }
 
     //other methods
 
-
+    /**
+     * calculatates the area of the rectangle
+     * @return area of the rectangle
+     */
+    public void calcArea(){
+        return height*width;
+    }
+    public void calcPerm(){
+        return (width*2)+(height*2);
+    }
 
 
     //toString method
